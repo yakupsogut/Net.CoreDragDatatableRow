9b05b0bdf3f7e08732de00644805c5aca844d035
Yakup Söğüt <yakup_sogut@hotmail.com>
2022-07-19 11:10:43 +0300

    Add project files.

---
+++ DragDatatablesRow/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using DragDatatablesRow.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DragDatatablesRow.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult DragTableRowsPostExample()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DragTableRowsPostExample(List<string> order, List<string> fullName)
+        {
+            //You can now process incoming data
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
